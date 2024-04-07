db3670386ad91681c44e32e347415eef8bd4f0b6
hyowon <phwworld@intense.co.kr>
2024-04-07 19:04:20 +0900

    first commit

---
+++ javascript/noona/newtime/index.js
@@ -0,0 +1,105 @@
+const apiKey = `b601dc8f4ca74300ae27c7d99e388c8d`;
+let newsList = [];
+const menus = document.querySelectorAll('.tab button');
+const searchBtn = document.querySelector('.search button');
+let url;
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
+menus.forEach((menu) =>
+    menu.addEventListener('click', (event) => getNewsByCategory(event))
+);
+
+searchBtn.addEventListener('click', () => getNewsByKeyword());
+
+const getNews = async () => {
+    try {
+        url.searchParams.set('page', page);
+        url.searchParams.set('pageSize', pageSize);
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        if (response.status == 200) {
+            if (data.articles.length === 0) {
+                throw new Error('검색된 내용이 없습니다.');
+            }
+            newsList = data.articles;
+            totalResults = data.totalResults;
+            render();
+            paginationRender();
+        } else {
+            throw new Error(data.message);
+        };
+    } catch (error) {
+        errorRender(error.message);
+    }
+}
+
+const getLatestNews = async () => {
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`);
+    getNews();
+}
+
+const getNewsByCategory = async (event) => {
+    let category = event.target.textContent.toLowerCase();
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${apiKey}`);
+    getNews();
+}
+
+const getNewsByKeyword = async () => {
+    let keyWord = document.querySelector('.search input').value;
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyWord}&apiKey=${apiKey}`);
+    getNews();
+}
+
+const render = () => {
+    const renderTarget = document.querySelector('.newsArea');
+    const newsHTML = newsList.map((item) => `
+    <div class="news">
+        <a href="${item.url}">
+            <div class="newsImg">
+                <img src="${item.urlToImage}" alt="">
+            </div>
+            <div class="newsTxt">
+                <p>${item.title}</p>
+                <p>${item.description}</p>
+                <p>${item.publishedAt}</p>
+            </div>
+        </a>
+    </div>
+    `).join('');
+    renderTarget.innerHTML = newsHTML;
+}
+
+const errorRender = (msg) => {
+    const renderTarget = document.querySelector('.newsArea');
+    const errorHtml = `<div>
+                        ${msg}
+                    </div>`;
+    renderTarget.innerHTML = errorHtml;
+}
+
+const paginationRender = () => {
+    let paginationHtml = ``;
+    const totalPages = Math.ceil(totalResults / pageSize); // 전체 페이징수
+    const pageGroup = Math.ceil(page / groupSize); // 한번에 노출되는 페이징수
+    let lastPage = pageGroup * groupSize; // 마지막 페이징
+    // 마지막 페이징이 pageGroup보다 작을 경우
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+    const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1); // 처음 페이징
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHtml += `<li><a href="javascript:;" onclick="movePage(${i})">${i}</a></li>`;
+    }
+    document.querySelector('.pagiNation').innerHTML = paginationHtml;
+}
+
+const movePage = (pageNum) => {
+    page = pageNum;
+    getNews();
+}
+
+getLatestNews();
---
+++ javascript/noona/number-guess/main.js
@@ -0,0 +1,59 @@
+let correctAnswer;
+let userInput = document.querySelector('.user-input');
+let checkBtn = document.querySelector('.check');
+let resetBtn = document.querySelector('.reset');
+let resultArea = document.querySelector('.result');
+let chanceArea = document.querySelector('.chance');
+let userAnswerList = [];
+let chance = 5;
+let gameOver = false;
+
+// 정답 만들기
+let makeCorrectAnswer = () => {
+    correctAnswer = Math.floor(Math.random() * 100) + 1;
+    console.log(correctAnswer);
+}
+
+// 게임 프로세스
+let play = () => {
+    userAnswer = userInput.value;
+    if (userAnswer < 1 || userAnswer > 100) {
+        resultArea.textContent = '1부터 100 사이의 숫자만 입력해 주세요.';
+        return;
+    } else if (userAnswerList.includes(userAnswer)) {
+        resultArea.textContent = '이미 입력한 숫자입니다.';
+        return;
+    } else {
+        if (userAnswer < correctAnswer) {
+            resultArea.textContent = 'Up';
+        } else if (userAnswer > correctAnswer) {
+            resultArea.textContent = 'Down';
+        } else {
+            resultArea.textContent = 'correct';
+        }
+        chance--;
+        chanceArea.innerHTML = `남은 기회 : ${chance}`;
+        userAnswerList.push(userAnswer);
+    }
+
+    if (chance < 1) {
+        gameOver = true;
+    }
+}
+
+// 게임 리셋
+let reset = () => {
+    makeCorrectAnswer();
+    chance = 5;
+    userInput.value = '';
+    chanceArea.innerHTML = `남은 기회 : ${chance}`;
+    userAnswerList = [];
+}
+
+// 확인 버튼 클릭
+checkBtn.addEventListener('click', play);
+
+// 리셋 버튼 클릭
+resetBtn.addEventListener('click', reset);
+
+makeCorrectAnswer();
